14a2e50dd3df7989dfc68c30023d36b2c3d9568e
Chris Arnesen <chris.arnesen@gmail.com>
2020-05-25 00:44:08 -0700

    Use eslint-config-carnesen@2.0

--- src/start.ts
+++ src/start.ts
@@ -1,9 +1,10 @@
+import { Server } from 'http';
 import { app } from './app';
 import { echo } from './util';
 
 const PORT = Number(process.env.PORT) || 8000;
 
-export function start(port = PORT) {
+export function start(port = PORT): Server {
   echo(`Starting server`);
   return app.listen(port, () => {
     echo(`Listening on port ${port}`);
