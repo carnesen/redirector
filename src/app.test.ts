696d119b36215a5f5b747f810e7cc9de4707299b
Chris Arnesen <chris.arnesen@gmail.com>
2020-05-28 01:40:16 -0700

    Fix async middleware to avoid timing issues

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,15 +1,15 @@
-import status = require('statuses');
+import { MOVED_PERMANENTLY, OK } from 'http-status-codes';
 import { app } from './app';
 const request = require('supertest');
 
 describe(__filename, () => {
   it('redirects to www', async () => {
     const res = await request(app.callback()).get('/user');
-    expect(res.status).toEqual(status('Moved Permanently'));
+    expect(res.status).toEqual(MOVED_PERMANENTLY);
     expect(res.headers.location).toEqual('https://www.127.0.0.1/user');
   });
   it('responds ok on health check', async () => {
     const res = await request(app.callback()).get('/_ah/start');
-    expect(res.status).toEqual(status('OK'));
+    expect(res.status).toEqual(OK);
   });
 });
