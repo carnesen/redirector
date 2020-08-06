693c4331c32452565e3ec47fee6dcad2619bbf23
Chris Arnesen <chris.arnesen@gmail.com>
2020-08-05 23:18:13 -0700

    Drop support for Node.js 10
    Update dependencies
    
    Move tests to __tests__ and exclude from package

--- src/app.ts
+++ src/app.ts
@@ -4,24 +4,24 @@ import { OK, MOVED_PERMANENTLY } from 'http-status-codes';
 import { echo } from './util';
 
 const loggerMiddleware: Koa.Middleware = async (ctx, next) => {
-  const message = `${ctx.method} ${ctx.url}`;
-  const startTimestamp = Date.now();
-  await next();
-  echo(`${ctx.status} ${message} - ${Date.now() - startTimestamp}ms`);
+	const message = `${ctx.method} ${ctx.url}`;
+	const startTimestamp = Date.now();
+	await next();
+	echo(`${ctx.status} ${message} - ${Date.now() - startTimestamp}ms`);
 };
 
 // Google Cloud App Engine-specific health check response
 const googleCloudAppEngineMiddleware: Koa.Middleware = async (ctx, next) => {
-  if (ctx.path === '/_ah/start') {
-    ctx.status = OK;
-  } else {
-    await next();
-  }
+	if (ctx.path === '/_ah/start') {
+		ctx.status = OK;
+	} else {
+		await next();
+	}
 };
 
 const redirectMiddleware: Koa.Middleware = (ctx) => {
-  ctx.status = MOVED_PERMANENTLY;
-  ctx.redirect(`https://www.${ctx.hostname}${ctx.path}`);
+	ctx.status = MOVED_PERMANENTLY;
+	ctx.redirect(`https://www.${ctx.hostname}${ctx.path}`);
 };
 
 const app = new Koa();
